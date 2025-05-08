2894dd668561e8c060033dfb6b6693c7a5549539
hydrocaeris <nurfatihsatria@gmail.com>
2025-05-08 16:07:24 +0800

    Initial commit

---
+++ cypress/e2e/verien-exalier/App.cy.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Login from "./components/Login"; // path harus benar
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Jayjay Cypress Student</h1>
+      <Login />
+    </div>
+  );
+}
